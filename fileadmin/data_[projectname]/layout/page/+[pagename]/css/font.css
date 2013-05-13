8a0d727efcc512c9191e4d3d77562fca14ae0b44
Janik von Rotz <contact@janikvonrotz.ch>
2013-05-13 16:50:57 +0200

    Init

---
+++ fileadmin/data_[projectname]/layout/page/+[pagename]/css/font.css
@@ -0,0 +1,16 @@
+/*
+#--------------------------------------------------#
+#    Title: [File-Title]
+#--------------------------------------------------#
+#    File-Type: [File-Type]
+#    Description: [Description]
+#    Project: [Projectname]
+# 
+#    Author: Janik von Rotz
+#    Author E-Mail: contact@janikvonrotz.ch
+#
+#    Create Date: dd.mm.yyyy hh.mm
+#    Last Edit Date: dd.mm.yyyy hh.mm
+#    Version: 0.0.0
+#--------------------------------------------------#
+*/
