eb7fd7d827611aae60fbd89dd01c21c3b4ec6c78
Janik von Rotz <contact@janikvonrotz.ch>
2013-06-03 09:07:06 +0200

    New stuff to migrate

---
+++ fileadmin/data_[projectname]/MIGRATE THIS/bin - indexed search/catdoc-0.94/src/ppt.h
@@ -0,0 +1,22 @@
+/**
+ * @file   ppt.h
+ * @author Alex Ott <alexott@gmail.com>
+ * @date   23 ‰≈À 2004
+ * Version: $Id: ppt.h,v 1.1 2005/03/15 20:06:51 vitus Exp $
+ * Copyright: Alex Ott
+ * 
+ * @brief  definitions of .ppt processing functions
+ * 
+ * 
+ */
+
+#ifndef _PPT_H
+#define _PPT_H 1
+
+#include <stdio.h>
+#include <math.h>
+
+void do_ppt(FILE *input,char *filename);
+
+#endif /* _PPT_H */
+
